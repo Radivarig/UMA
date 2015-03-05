ad981c2a57966b4fb1029263a8e06b437bb54177
ecurtz <eli@nuprometheus.com>
2015-03-05 12:10:11 -0800

    Disabled the PRO check for texture combiner enforcement in Unity 5

--- UMAProject/Assets/UMA/Example/Scripts/UMAGenerator.cs
+++ UMAProject/Assets/UMA/Example/Scripts/UMAGenerator.cs
@@ -5,6 +5,7 @@ public class UMAGenerator : UMAGeneratorBuiltin
 {
 	public override void Awake()
 	{
+#if !UNITY_5
 		if (usePRO)
 		{
 #if UNITY_EDITOR
@@ -13,8 +14,10 @@ public class UMAGenerator : UMAGeneratorBuiltin
 				Debug.LogError("You told the Generator to usePRO but you don't have pro license. Toggling to false.", gameObject);
 				usePRO = false;
 			}
+
 #endif
 		}
+#endif
 		base.Awake();
 	}
 
